752774c538bc93269195e0a43521f4ea1cb0f400
Shrey Sachdeva <Shrey Sachdeva@ShreySachdeva>
2025-04-24 10:44:57 +0530

    first commit.

---
+++ src/JavaCoding/morejavacoding.java
@@ -0,0 +1,10 @@
+package JavaCoding;
+
+public class morejavacoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a simple java program");
+	}
+
+}
